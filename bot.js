b21bdd4d0e22958d544484271ab167d6ffc5af76
Gil Lohner <gil@lohner.net>
2024-09-21 02:48:17 +0200

    add community links

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const {
   handleMeetups,
   handleRefreshCommands
 } = require('./handlers');
+const communityLinks = require('./communityLinks');
 
 const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, {
   polling: true
@@ -16,6 +17,37 @@ const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, {
 bot.onText(/\/start/, (msg) => handleStart(bot, msg));
 bot.onText(/\/meetups/, (msg) => handleMeetups(bot, msg));
 bot.onText(/\/refresh_commands/, (msg) => handleRefreshCommands(bot, msg));
+bot.onText(/\/links/, (msg) => {
+  const chatId = msg.chat.id;
+  const keyboard = {
+    inline_keyboard: Object.keys(communityLinks).map(category => [{
+      text: category,
+      callback_data: `links_${category}`
+    }])
+  };
+  bot.sendMessage(chatId, 'Wähle eine Kategorie:', {
+    reply_markup: JSON.stringify(keyboard)
+  });
+});
+bot.on('callback_query', async (callbackQuery) => {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const chatId = msg.chat.id;
+
+  if (action.startsWith('links_')) {
+    const category = action.split('_')[1];
+    const links = communityLinks[category];
+    let message = `${category}:\n\n`;
+    links.forEach(link => {
+      message += `${link.name}\n${link.url}\n\n`;
+    });
+    await bot.answerCallbackQuery(callbackQuery.id);
+    await bot.sendMessage(chatId, message, {
+      parse_mode: 'HTML',
+      disable_web_page_preview: true
+    });
+  }
+});
 
 async function main() {
   console.log('Bot is starting...');
---
+++ communityLinks.js
@@ -0,0 +1,67 @@
+const communityLinks = {
+  'Community Gruppen': [{
+      name: 'Bitcoin Gruppe 💬',
+      url: 'https://t.me/+iu9fNXw0NeE4ZmE0'
+    },
+    {
+      name: 'Meetup Gruppe 🍻',
+      url: 'https://schweiz.einundzwanzig.space'
+    },
+    {
+      name: 'Offtopic Gruppe 👀',
+      url: 'https://t.me/joinchat/NL5C5XuMSwczMzJh'
+    },
+    {
+      name: 'Website 🔗',
+      url: 'https://dezentralschweiz.ch'
+    }
+  ],
+  'Community Links': [{
+      name: 'Podcast 🎙',
+      url: 'https://feeds.rssblue.com/dezentralschweiz'
+    },
+    {
+      name: 'Dezentralshop 🛒',
+      url: 'https://dezentralshop.ch'
+    },
+    {
+      name: 'Education B',
+      url: '@CHeducationBTC'
+    },
+    {
+      name: 'Bitcoin Mentoring',
+      url: 'https://www.bitcoin-mentoring.me'
+    },
+    {
+      name: 'Lipa',
+      url: 'https://lipa.swiss'
+    },
+    {
+      name: 'Bitcoin Reise',
+      url: 'https://youtube.com/@bitcoinreise?si=ZQ6GmiUoAXPRw9Cs'
+    },
+    {
+      name: 'Funtoshi',
+      url: 'https://funtoshi.com'
+    },
+    {
+      name: 'Berglinde Newsletter',
+      url: 'https://news.berglinde.com/'
+    }
+  ],
+  'Vereine': [{
+      name: 'Bitcoin Baden',
+      url: 'https://bitcoinbaden.ch'
+    },
+    {
+      name: 'Bitcoin Alps (Thun)',
+      url: 'https://www.bitcoin-alps.ch'
+    },
+    {
+      name: 'Bitcoin Association Switzerland (BAS)',
+      url: 'https://www.bitcoinassociation.ch'
+    }
+  ]
+};
+
+module.exports = communityLinks;
