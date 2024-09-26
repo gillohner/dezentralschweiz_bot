1c0a8ba12def4cd1b487bed9b736944d4f7f256e
Gil Lohner <gil@lohner.net>
2024-09-26 21:27:31 +0200

    fixed meetup creation process

---
+++ telegramGroups.js
@@ -0,0 +1,29 @@
+const telegramGroups = {
+  '-4520304388': [
+    title: "Fake Admin Group",
+    welcomeMessage: "hallo"
+  ],
+  '-1001750710633': [
+    title: "Dezentralschweiz - Offtopic 👀",
+    welcomeMessage: "
+<b>Willkommen in der Dezentralschweiz Offtopic Gruppe! 👀</b>
+
+In dieser Gruppe dürfen gerne mal Themen abseits von Bitcoin diskutiert und besprochen werden.
+
+Falls du dich jedoch auf Bitcoin fokussieren möchtest, führen wir des Weiteren noch eine Bitcoin, sowie eine Meetup Gruppe:
+
+Bitcoin Gruppe: t.me/+ETWFIUzxnlNkZDg0
+Meetup Gruppe: schweiz.einundzwanzig.space
+"
+  ],
+  '-1001886830711': [
+    title: "Bitcoin Luzern",
+    welcomeMessage: "hallo"
+  ],
+  '-1001759384231': [
+    title: "Dezentralschweiz - Bitcoin 💬",
+    welcomeMessage: "hallo"
+  ],
+};
+
+export default telegramGroups;
