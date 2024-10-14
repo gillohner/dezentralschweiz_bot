0fea2075f7f9aada3a6901bcc9793f726d764f4f
gil <gil.lohner@i-web.ch>
2024-10-14 18:28:36 +0200

    prevent updating meetup before new one is sent

--- handlers/adminApprovalHandler.js
+++ handlers/adminApprovalHandler.js
@@ -9,12 +9,12 @@ import {
 } from '../scheduledTasks.js'
 
 const handleAdminApproval = async (bot, callbackQuery, action) => {
-    postWeeklyMeetups(bot);
     if (action.startsWith('approve_delete_') || action.startsWith('reject_delete_')) {
-        handleAdminMeetupDeletionApproval(bot, callbackQuery);
+        await handleAdminMeetupDeletionApproval(bot, callbackQuery);
     } else if (action.startsWith('approve_meetup_') || action.startsWith('reject_meetup_')) {
         await handleAdminMeetupSuggestionApproval(bot, callbackQuery);
     }
+    postWeeklyMeetups(bot);
 };
 
 export {
