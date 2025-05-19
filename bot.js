162468827901b1b09b32508dd862d9bb7076ea25
Gil Lohner <gil@lohner.net>
2025-05-19 18:37:40 +0200

    fix spamming

--- bot.js
+++ bot.js
@@ -1,29 +1,29 @@
 // External Libraries
-import TelegramBot from 'node-telegram-bot-api';
+import TelegramBot from "node-telegram-bot-api";
 
 // Config
-import config from './bot/config.js';
+import config from "./bot/config.js";
 
 // Import Tasks
-import { scheduleWeeklyMeetupPost } from "./scheduledTasks.js"
+import { scheduleWeeklyMeetupPost } from "./scheduledTasks.js";
 
 // Handlers
-import { setupCommands } from './bot/commands.js';
-import { handleMeetupSuggestion } from './handlers/meetupHandlers/meetupSuggestionHandler.js';
-import { handleMeetups } from './handlers/meetupHandlers/meetupDisplayingHandler.js';
-import { handleLinks } from './handlers/linkHandler.js';
-import { handleCallbackQuery } from './handlers/callbackHandler.js';
-import { handleRefreshCommands } from './handlers/refreshCommandsHandler.js';
-import { handleMeetupDeletion } from './handlers/meetupHandlers/meetupDeletionHandler.js';
-import { handleNewMember } from './handlers/newMemberHandler.js';
-import { handleMessage } from './handlers/messageHandler.js';
-import { handleStart } from './handlers/startHandler.js';
+import { setupCommands } from "./bot/commands.js";
+import { handleMeetupSuggestion } from "./handlers/meetupHandlers/meetupSuggestionHandler.js";
+import { handleMeetups } from "./handlers/meetupHandlers/meetupDisplayingHandler.js";
+import { handleLinks } from "./handlers/linkHandler.js";
+import { handleCallbackQuery } from "./handlers/callbackHandler.js";
+import { handleRefreshCommands } from "./handlers/refreshCommandsHandler.js";
+import { handleMeetupDeletion } from "./handlers/meetupHandlers/meetupDeletionHandler.js";
+import { handleNewMember } from "./handlers/newMemberHandler.js";
+import { handleMessage } from "./handlers/messageHandler.js";
+import { handleStart } from "./handlers/startHandler.js";
 
 // Datasets
-import communityLinks from './datasets/communityLinks.js';
+import communityLinks from "./datasets/communityLinks.js";
 
 const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, {
-  polling: true
+  polling: true,
 });
 
 const initializeBot = async (bot) => {
@@ -31,7 +31,7 @@ const initializeBot = async (bot) => {
     const botInfo = await bot.getMe();
     bot.username = botInfo.username;
   } catch (error) {
-    console.error('Error initializing bot:', error);
+    console.error("Error initializing bot:", error);
   }
 };
 
@@ -42,18 +42,19 @@ const setupEventHandlers = (bot) => {
   bot.onText(/\/links/, (msg) => handleLinks(bot, msg, communityLinks));
   bot.onText(/\/meetup_vorschlagen/, (msg) => handleMeetupSuggestion(bot, msg));
   bot.onText(/\/meetup_loeschen/, (msg) => handleMeetupDeletion(bot, msg));
-  bot.on('new_chat_members', (msg) => handleNewMember(bot, msg));
-  bot.on('message', (msg) => handleMessage(bot, msg));
-  bot.on('callback_query', (callbackQuery) => handleCallbackQuery(bot, callbackQuery));
+  bot.on("new_chat_members", (msg) => handleNewMember(bot, msg));
+  bot.on("message", (msg) => handleMessage(bot, msg));
+  bot.on("callback_query", (callbackQuery) =>
+    handleCallbackQuery(bot, callbackQuery)
+  );
   bot.on("polling_error", (error) => console.log("Polling error:", error));
 };
 
 const main = async () => {
-  console.log('Booting up the Nostr...');
+  console.log("Booting up the Nostr...");
   await setupCommands(bot);
   await initializeBot(bot);
   setupEventHandlers(bot);
-  scheduleWeeklyMeetupPost(bot);
 };
 
 main();
--- handlers/adminApprovalHandler.js
+++ handlers/adminApprovalHandler.js
@@ -1,22 +1,18 @@
-import {
-    handleAdminMeetupDeletionApproval
-} from './meetupHandlers/meetupDeletionHandler.js'
-import {
-    handleAdminMeetupSuggestionApproval
-} from './meetupHandlers/meetupSuggestionHandler.js'
-import {
-    postWeeklyMeetups
-} from '../scheduledTasks.js'
+import { handleAdminMeetupDeletionApproval } from "./meetupHandlers/meetupDeletionHandler.js";
+import { handleAdminMeetupSuggestionApproval } from "./meetupHandlers/meetupSuggestionHandler.js";
 
 const handleAdminApproval = async (bot, callbackQuery, action) => {
-    if (action.startsWith('approve_delete_') || action.startsWith('reject_delete_')) {
-        await handleAdminMeetupDeletionApproval(bot, callbackQuery);
-    } else if (action.startsWith('approve_meetup_') || action.startsWith('reject_meetup_')) {
-        await handleAdminMeetupSuggestionApproval(bot, callbackQuery);
-    }
-    postWeeklyMeetups(bot);
+  if (
+    action.startsWith("approve_delete_") ||
+    action.startsWith("reject_delete_")
+  ) {
+    await handleAdminMeetupDeletionApproval(bot, callbackQuery);
+  } else if (
+    action.startsWith("approve_meetup_") ||
+    action.startsWith("reject_meetup_")
+  ) {
+    await handleAdminMeetupSuggestionApproval(bot, callbackQuery);
+  }
 };
 
-export {
-    handleAdminApproval
-};
+export { handleAdminApproval };
