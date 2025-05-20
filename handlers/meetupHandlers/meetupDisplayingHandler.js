76188589ab8fee9d523d3594c88a1c655a09359f
Gil Lohner <gil@lohner.net>
2025-05-20 16:46:57 +0200

    update url

--- handlers/meetupHandlers/meetupDisplayingHandler.js
+++ handlers/meetupHandlers/meetupDisplayingHandler.js
@@ -1,212 +1,227 @@
 import userStates from "../../userStates.js";
-import config from '../../bot/config.js';
+import config from "../../bot/config.js";
 import {
-    extractTelegramUsername,
-    formatLocation,
-    formatDate,
-    escapeHTML,
-    extractMapLinks
-} from '../../utils/helpers.js'
-import {
-    checkForDeletionEvent
-} from '../../utils/nostrUtils.js';
-import {
-    nip19
-} from 'nostr-tools';
-import {
-    deleteMessageWithTimeout,
-    sendAndStoreMessage,
-    deleteMessage,
-    editAndStoreMessage
+  extractTelegramUsername,
+  formatLocation,
+  formatDate,
+  escapeHTML,
+  extractMapLinks,
 } from "../../utils/helpers.js";
+import { checkForDeletionEvent } from "../../utils/nostrUtils.js";
+import { nip19 } from "nostr-tools";
 import {
-    fetchAndProcessEvents
-} from "../../utils/eventUtils.js";
-import { getTimeFrameName, getCallbackData } from '../../utils/timeFrameUtils.js';
-import url from 'url';
-
+  deleteMessageWithTimeout,
+  sendAndStoreMessage,
+  deleteMessage,
+  editAndStoreMessage,
+} from "../../utils/helpers.js";
+import { fetchAndProcessEvents } from "../../utils/eventUtils.js";
+import {
+  getTimeFrameName,
+  getCallbackData,
+} from "../../utils/timeFrameUtils.js";
+import url from "url";
 
 const handleMeetups = async (bot, msg) => {
-    const chatId = msg.chat.id;
-    const timeFrames = ['heute', 'dieseWoche', '7Tage', '30Tage', 'alle'];
-    const keyboard = {
-        inline_keyboard: timeFrames.map(timeFrame => [{
-            text: getTimeFrameName(timeFrame),
-            callback_data: getCallbackData(timeFrame)
-        }])
-    };
+  const chatId = msg.chat.id;
+  const timeFrames = ["heute", "dieseWoche", "7Tage", "30Tage", "alle"];
+  const keyboard = {
+    inline_keyboard: timeFrames.map((timeFrame) => [
+      {
+        text: getTimeFrameName(timeFrame),
+        callback_data: getCallbackData(timeFrame),
+      },
+    ]),
+  };
 
-    // Delete the user's /meetup command message
-    deleteMessage(bot, chatId, msg.message_id);
+  // Delete the user's /meetup command message
+  deleteMessage(bot, chatId, msg.message_id);
 
-    // Delete previous meetup message
-    if (userStates[chatId]?.lastMeetupMessageId) {
-        deleteMessage(bot, chatId, userStates[chatId].lastMeetupMessageId);
-    }
-    const sentMessage = await sendAndStoreMessage(
-        bot,
-        chatId,
-        'Wähle den Zeitraum für die Meetups:', {
-        reply_markup: JSON.stringify(keyboard),
-        disable_notification: true
+  // Delete previous meetup message
+  if (userStates[chatId]?.lastMeetupMessageId) {
+    deleteMessage(bot, chatId, userStates[chatId].lastMeetupMessageId);
+  }
+  const sentMessage = await sendAndStoreMessage(
+    bot,
+    chatId,
+    "Wähle den Zeitraum für die Meetups:",
+    {
+      reply_markup: JSON.stringify(keyboard),
+      disable_notification: true,
     },
-        'lastMeetupMessageId'
-    );
-    deleteMessageWithTimeout(bot, chatId, sentMessage.message_id);
+    "lastMeetupMessageId"
+  );
+  deleteMessageWithTimeout(bot, chatId, sentMessage.message_id);
 };
 
-const handleMeetupsFilter = async (bot, msg, timeFrame, returnMessage = "lastMeetupMessageId") => {
-    const chatId = msg.chat.id;
+const handleMeetupsFilter = async (
+  bot,
+  msg,
+  timeFrame,
+  returnMessage = "lastMeetupMessageId"
+) => {
+  const chatId = msg.chat.id;
 
-    try {
-        if (userStates[chatId]?.lastMeetupMessageId) {
-            await deleteMessage(bot, chatId, userStates[chatId].lastMeetupMessageId);
-        }
-
-        const loadingMessage = await bot.sendMessage(chatId, 'Mining new Meetups, bitte warten...', {
-            disable_notification: true
-        });
-
-        const meetupMessage = await fetchMeetupsLogic(timeFrame);
-
-        const sentMessage = await editAndStoreMessage(
-            bot,
-            chatId,
-            meetupMessage,
-            {
-                chat_id: chatId,
-                message_id: loadingMessage.message_id,
-                parse_mode: 'HTML',
-                disable_web_page_preview: true,
-                disable_notification: true,
-            },
-            returnMessage
-        );
-
-        if (returnMessage === 'lastMeetupMessageId') {
-            deleteMessageWithTimeout(bot, chatId, sentMessage.message_id);
-        }
-
-        return returnMessage ? meetupMessage : undefined;
-    } catch (error) {
-        console.error('Error in handleMeetupsFilter:', error);
-        const errorMessageText = 'Ein Fehler ist beim Mining der Meetups aufgetreten. Bitte versuche es später erneut.';
-
-        if (returnMessage) {
-            return errorMessageText;
-        }
-
-        const errorMessage = await sendAndStoreMessage(
-            bot,
-            chatId,
-            errorMessageText,
-            {
-                disable_notification: true
-            },
-            returnMessage
-        );
-
-        deleteMessageWithTimeout(bot, chatId, errorMessage.message_id);
+  try {
+    if (userStates[chatId]?.lastMeetupMessageId) {
+      await deleteMessage(bot, chatId, userStates[chatId].lastMeetupMessageId);
     }
-};
 
+    const loadingMessage = await bot.sendMessage(
+      chatId,
+      "Mining new Meetups, bitte warten...",
+      {
+        disable_notification: true,
+      }
+    );
+
+    const meetupMessage = await fetchMeetupsLogic(timeFrame);
+
+    const sentMessage = await editAndStoreMessage(
+      bot,
+      chatId,
+      meetupMessage,
+      {
+        chat_id: chatId,
+        message_id: loadingMessage.message_id,
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+        disable_notification: true,
+      },
+      returnMessage
+    );
+
+    if (returnMessage === "lastMeetupMessageId") {
+      deleteMessageWithTimeout(bot, chatId, sentMessage.message_id);
+    }
+
+    return returnMessage ? meetupMessage : undefined;
+  } catch (error) {
+    console.error("Error in handleMeetupsFilter:", error);
+    const errorMessageText =
+      "Ein Fehler ist beim Mining der Meetups aufgetreten. Bitte versuche es später erneut.";
+
+    if (returnMessage) {
+      return errorMessageText;
+    }
+
+    const errorMessage = await sendAndStoreMessage(
+      bot,
+      chatId,
+      errorMessageText,
+      {
+        disable_notification: true,
+      },
+      returnMessage
+    );
+
+    deleteMessageWithTimeout(bot, chatId, errorMessage.message_id);
+  }
+};
 
 const formatMeetupsMessage = async (allEvents, timeFrame) => {
-    let message = `🍻 <b>${getHeaderMessage(timeFrame)}</b> 🍻\n\n`;
+  let message = `🍻 <b>${getHeaderMessage(timeFrame)}</b> 🍻\n\n`;
 
-    for (const {
-        calendarName,
-        events,
-        naddr
-    } of allEvents) {
-        if (events.length > 0) {
-            const calendarUrl = `https://www.flockstr.com/calendar/${naddr}`;
-            message += `<b>📅 <a href="${calendarUrl}">${escapeHTML(calendarName)}</a></b>\n\n`;
+  for (const { calendarName, events, naddr } of allEvents) {
+    if (events.length > 0) {
+      const calendarUrl = `https://dezentralbot.riginode.xyz/calendar/${naddr}`;
+      message += `<b>📅 <a href="${calendarUrl}">${escapeHTML(
+        calendarName
+      )}</a></b>\n\n`;
 
-            for (let i = 0; i < events.length; i++) {
-                const event = events[i];
+      for (let i = 0; i < events.length; i++) {
+        const event = events[i];
 
-                if (await checkForDeletionEvent(event.id)) continue;
+        if (await checkForDeletionEvent(event.id)) continue;
 
-                const title = event.tags.find(t => t[0] === 'name')?.[1] || event.tags.find(t => t[0] === 'title')?.[1];
-                if (!title) continue;
+        const title =
+          event.tags.find((t) => t[0] === "name")?.[1] ||
+          event.tags.find((t) => t[0] === "title")?.[1];
+        if (!title) continue;
 
-                const start = event.tags.find(t => t[0] === 'start')?.[1];
-                if (!start) continue;
+        const start = event.tags.find((t) => t[0] === "start")?.[1];
+        if (!start) continue;
 
-                const end = event.tags.find(t => t[0] === 'end')?.[1];
+        const end = event.tags.find((t) => t[0] === "end")?.[1];
 
-                const locationTag = event.tags.find(t => t[0] === 'location');
-                const location = locationTag ? locationTag[1] : null;
-                const { googleMapsLink, osmLink, appleMapsLink } = extractMapLinks(event.tags);
-                console.log(event.tags)
-                console.log("Google Maps Link:", googleMapsLink);
-                console.log("OSM Link:", osmLink);
-                console.log("Apple Maps Link:", appleMapsLink);
+        const locationTag = event.tags.find((t) => t[0] === "location");
+        const location = locationTag ? locationTag[1] : null;
+        const { googleMapsLink, osmLink, appleMapsLink } = extractMapLinks(
+          event.tags
+        );
+        console.log(event.tags);
+        console.log("Google Maps Link:", googleMapsLink);
+        console.log("OSM Link:", osmLink);
+        console.log("Apple Maps Link:", appleMapsLink);
 
-                const eventNaddr = nip19.naddrEncode({
-                    kind: event.kind,
-                    pubkey: event.pubkey,
-                    identifier: event.tags.find(t => t[0] === 'd')?.[1] || '',
-                });
-                const eventUrl = `https://www.flockstr.com/event/${eventNaddr}`;
+        const eventNaddr = nip19.naddrEncode({
+          kind: event.kind,
+          pubkey: event.pubkey,
+          identifier: event.tags.find((t) => t[0] === "d")?.[1] || "",
+        });
+        const eventUrl = `https://dezentralbot.riginode.xyz/event/${eventNaddr}`;
 
-                message += `🎉 <b><a href="${eventUrl}">${escapeHTML(title)}</a></b>\n`;
-                if (start) {
-                    message += `🕒 <b>${formatDate(parseInt(start) * 1000)}</b>`;
-                    if (end) message += ` - ${formatDate(parseInt(end) * 1000)}`;
-                    message += `\n`;
-                }
-
-                const telegramUser = extractTelegramUsername(event.tags);
-                if (telegramUser) {
-                    message += `👤 ${escapeHTML(telegramUser)}\n`;
-                }
-
-                if (location) {
-                    message += await formatLocation(location, googleMapsLink, osmLink, appleMapsLink);
-                }
-
-                // Add separator only if this is not the last event
-                if (i < events.length - 1) {
-                    message += '\n🔶♦️🔶♦️🔶♦️🔶♦️🔶♦️🔶♦️🔶\n\n';
-                }
-            }
+        message += `🎉 <b><a href="${eventUrl}">${escapeHTML(title)}</a></b>\n`;
+        if (start) {
+          message += `🕒 <b>${formatDate(parseInt(start) * 1000)}</b>`;
+          if (end) message += ` - ${formatDate(parseInt(end) * 1000)}`;
+          message += `\n`;
         }
-    }
 
-    return message;
+        const telegramUser = extractTelegramUsername(event.tags);
+        if (telegramUser) {
+          message += `👤 ${escapeHTML(telegramUser)}\n`;
+        }
+
+        if (location) {
+          message += await formatLocation(
+            location,
+            googleMapsLink,
+            osmLink,
+            appleMapsLink
+          );
+        }
+
+        // Add separator only if this is not the last event
+        if (i < events.length - 1) {
+          message += "\n🔶♦️🔶♦️🔶♦️🔶♦️🔶♦️🔶♦️🔶\n\n";
+        }
+      }
+    }
+  }
+
+  return message;
 };
 
 const fetchMeetupsLogic = async (timeFrame) => {
-    const result = await fetchAndProcessEvents(config, timeFrame);
+  const result = await fetchAndProcessEvents(config, timeFrame);
 
-    if (result.status === 'empty' || result.status === 'noEvents') {
-        return result.message;
-    }
+  if (result.status === "empty" || result.status === "noEvents") {
+    return result.message;
+  }
 
-    return await formatMeetupsMessage(result.events, timeFrame);
+  return await formatMeetupsMessage(result.events, timeFrame);
 };
 
 const getHeaderMessage = (timeFrame) => {
-    switch (timeFrame) {
-        case 'heute':
-            return 'Meetups heute';
-        case 'dieseWoche':
-            return 'Meetups diese Woche';
-        case '7Tage':
-            return 'Meetups in den nächsten 7 Tagen';
-        case '30Tage':
-            return 'Meetups in den nächsten 30 Tagen';
-        default:
-            return 'Alle bevorstehenden Meetups';
-    }
+  switch (timeFrame) {
+    case "heute":
+      return "Meetups heute";
+    case "dieseWoche":
+      return "Meetups diese Woche";
+    case "7Tage":
+      return "Meetups in den nächsten 7 Tagen";
+    case "30Tage":
+      return "Meetups in den nächsten 30 Tagen";
+    default:
+      return "Alle bevorstehenden Meetups";
+  }
 };
 
 export {
-    handleMeetups,
-    handleMeetupsFilter,
-    formatMeetupsMessage,
-    getHeaderMessage,
-    fetchMeetupsLogic,
+  handleMeetups,
+  handleMeetupsFilter,
+  formatMeetupsMessage,
+  getHeaderMessage,
+  fetchMeetupsLogic,
 };
